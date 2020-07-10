ab5ad89f6a1f34e7c399e6e1afbff02f8b79c1bb
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-07-10 17:23:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Gulp Boilerplate
+
+A simple boilerplate for front-end web development which uses [Gulp](https://gulpjs.com/) v4.
+
+## Features
+
+- Live reloading
+- Cache busting
+- SCSS converted to css, auto prefixed and minified with sourcemaps)
+- Javascript concatenated into a single file, minified with sourcemaps and supports ES6)
+- Image minifying
+- SCSS Normalize/Reset (default is Normalize)
+
+## Getting Started
+
+Follow these steps in order to get the website up and running locally on your machine.
+
+### Installation
+
+- `npm install` to install any dependencies
+- `npm start` or `gulp watch` to start a live reload session
+
+### Building
+
+- `npm run build` or `gulp` to build the application
+
+### Extras
+
+- `gulp` or `gulp build` to build the application
+- `gulp watch` to enable live reload
+- `gulp clean` to delete the build folder
+- `gulp styles` to run the style tasks
+- `gulp scripts` to run the script tasks
+- `gulp images` to run the image tasks
+- `gulp favicon` to run the favicon tasks
+
+## Tips
+
+- You can use any file structure for your javascript and scss files. The one provided is an example.
---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const replace = require("gulp-replace");
+const imagemin = require("gulp-imagemin");
+const plumber = require("gulp-plumber");
+
+const paths = {
+    html: {
+        src: "./app/**/*.html",
+        dest: "./build",
+    },
+    styles: {
+        src: "./app/scss/**/*.scss",
+        dest: "./build/assets/css",
+    },
+    scripts: {
+        src: "./app/js/**/*.js",
+        dest: "./build/assets/js",
+    },
+    images: {
+        src: "./app/images/**/*",
+        dest: "./build/assets/images",
+    },
+    favicon: {
+        src: "./app/favicon.ico",
+        dest: "./build",
+    },
+};
+
+const clean = () => del(["./build"]);
+
+// Cache busting to prevent browser caching issues
+const curTime = new Date().getTime();
+const cacheBust = () =>
+    gulp
+        .src(paths.html.src)
+        .pipe(plumber())
+        .pipe(replace(/cb=\d+/g, "cb=" + curTime))
+        .pipe(gulp.dest(paths.html.dest));
+
+// Copies all html files
+const html = () =>
+    gulp.src(paths.html.src).pipe(plumber()).pipe(gulp.dest(paths.html.dest));
+
+// Convert scss to css, auto-prefix, minify and rename into styles.min.css
+const styles = () =>
+    gulp
+        .src(paths.styles.src)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                includePaths: require("scss-resets").includePaths,
+            }).on("error", sass.logError)
+        )
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(
+            rename({
+                basename: "styles",
+                suffix: ".min",
+            })
+        )
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+
+// Minify all javascript files and concat them into a single app.min.js
+const scripts = () =>
+    gulp
+        .src(paths.scripts.src)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(
+            babel({
+                presets: ["@babel/preset-env"],
+            })
+        )
+        .pipe(terser())
+        .pipe(concat("app.min.js"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.scripts.dest));
+
+// Copy and minify images
+const images = () =>
+    gulp
+        .src(paths.images.src)
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
+
+// Copy the favicon
+const favicon = () =>
+    gulp
+        .src(paths.favicon.src)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.favicon.dest));
+
+// Watches all .scss, .js and .html changes and executes the corresponding task
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./build",
+        },
+        notify: false,
+    });
+
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.favicon.src, favicon).on("change", browserSync.reload);
+    gulp.watch(paths.scripts.src, scripts).on("change", browserSync.reload);
+    gulp.watch(paths.images.src, images).on("change", browserSync.reload);
+    gulp.watch(paths.html.src, html).on("change", browserSync.reload);
+};
+
+const build = gulp.series(
+    clean,
+    gulp.parallel(styles, scripts, images, favicon),
+    cacheBust
+);
+
+const watch = gulp.series(build, watchFiles);
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.favicon = favicon;
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
